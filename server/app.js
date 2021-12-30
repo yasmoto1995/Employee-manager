d88d8141ca49bcbf808796ee858da5a349924f7f
yasmoto <yasmoto1995@gmail.com>
2021-12-30 23:45:02 +0530

    Store data to mongo on POST successfull

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 var express = require('express');
-var ejs = require('ejs');
 const cors = require('cors');
 var bodyParser = require('body-parser')
+let MongoClient = require('mongodb').MongoClient;
+
+var url = "mongodb+srv://user:if4zEWLcMZsqnKY@employeedb.qwpku.mongodb.net/EmployeeDB?retryWrites=true&w=majority";
 
 const PORT = 7000;
 
@@ -17,9 +19,33 @@ app.use(
 }));
 
 app.post("/add", function (req, res) {
-
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("EmployeeDB");
+    dbo.collection("employees").insertOne({
+      firstName: req.body.firstName, 
+      lastName: req.body.lastName, 
+      email: req.body.email, 
+      contact: req.body.contact, 
+      address: req.body.address, 
+      dept: req.body.dept, 
+      role: req.body.role, 
+      city: req.body.city, 
+      state: req.body.state, 
+      zip: req.body.zip, 
+      country: req.body.country, 
+      pass: req.body.pass 
+    }, 
+    function(err, result) {
+        if (err) throw err;
+        res.json(result);
+        db.close();
+    });
+  });
 });
 
+
+//mongodb+srv://user:if4zEWLcMZsqnKY@employeedb.qwpku.mongodb.net/EmployeeDB?retryWrites=true&w=majority
 
 app.listen(PORT, function (err) {
   if (err) console.log(err);
